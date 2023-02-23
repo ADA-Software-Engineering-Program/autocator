044fa351321fae246c9e593bbaa7b3ee6f64d861
[Lawrencmagnat] <[chukwulawrencep@gmail.com]>
2023-02-23 20:30:35 +0100

    The commit m

--- my_autocard/src/App.js
+++ my_autocard/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   <h1>Hello..Team Autocator</h1>
+   <h1>Hello..Team Autocator, We will get back to u</h1>
     </div>
   );
 }
